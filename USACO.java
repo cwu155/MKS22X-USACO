f9cc7b7e36772b2dd0ffb618528cf7bddf932203
cwu155 <cwu17@stuy.edu>
2019-03-11 01:11:27 -0400

    Now reads instructions

--- USACO.java
+++ USACO.java
@@ -4,9 +4,12 @@ import java.io.*;
 public class USACO{
 
   public static int[][] map;
-  public static int r, c, e, n, rows, cols;
+  public static int[][] cowGrid;
+  public static int[][] instructions;
+  public static int r, c, e, n, R_s, C_s;
   public static String line;
 
+  //Helper method for reading file.
   public static int[] stringToInt(String input){
     String[] stringArray = input.split(" ");
     int[] result = new int[stringArray.length];
@@ -17,40 +20,44 @@ public class USACO{
     return result;
   }
 
+  //Reads file, assigns variables.
   public static void readFile(String filename) throws FileNotFoundException, IOException{
     try {
+      //Scanners and readers and everything??
       File text = new File(filename);
       Scanner inf = new Scanner(text);
-
       BufferedReader brTest = new BufferedReader(new FileReader(filename));
       String firstLine = brTest.readLine();
       String[] firstLineArray = new String[4];
       firstLineArray = firstLine.split(" ");
+
+      //Determines rows, cols, elevation, number of instructions.
       r = Integer.parseInt(firstLineArray[0]);
       c = Integer.parseInt(firstLineArray[1]);
       e = Integer.parseInt(firstLineArray[2]);
       n = Integer.parseInt(firstLineArray[3]);
 
+      //Initializes map.
       map = new int[r][c];
-
       inf.nextLine();
-      for (int i = 0; i < r; i ++) {
+      for (int i = 0; i < r; i ++){
         for (int j = 0; j < c; j++) {
           map[i][j] = inf.nextInt();
         }
       }
+      //System.out.println(inf.nextLine());
+      //System.out.println(inf.nextLine());
 
-      // File text1 = new File(filename);
-      // Scanner inf1 = new Scanner(text1);
-      //
-      // inf1.nextLine();
-      //    while(inf1.hasNextLine()){
-      //      String next = inf1.nextLine();
-      //      map[r] = stringToInt(next);
-      //      if (map[r].length == 3){;}
-      //      r += 1;
-      //    }
+      //Instructions.
 
+      instructions = new int[n][3];
+      for (int i = 0; i < n; i++){
+        inf.nextLine();
+        for (int j = 0; j < 3; j++){
+        instructions[i][j] = inf.nextInt();
+      }
+    }
+      //Exceptions.
       } catch (FileNotFoundException ex){
         System.out.println("Yikes");
       } catch (IOException ex){
@@ -58,25 +65,30 @@ public class USACO{
       }
   }
 
-    //Debug purposes
-    public static String toString(int[][] map){
+    //Debug purposes.
+    public static String toString(int[][] grid){
       String result = "";
-      for (int i = 0; i < map.length; i++){
-          for (int j = 0; j < map[i].length; j++){
+      for (int i = 0; i < grid.length; i++){
+          for (int j = 0; j < grid[i].length; j++){
 
-            if (j == map[i].length - 1){
-              result += map[i][j] + "\n";
+            if (j == grid[i].length - 1){
+              result += grid[i][j] + "\n";
               } else {
-              result += map[i][j] + " ";
+              result += grid[i][j] + " ";
               }
             }
           }
         return result;
       }
 
+    //Initializes 3x3 cow grid.
+    public static void initCowGrid(int Rs, int Cs){
+      cowGrid = new int[3][3];
+    }
 
     public static void main(String[] args)throws FileNotFoundException, IOException{
       readFile("makelake.in");
       System.out.println(toString(map));
+      System.out.println(toString(instructions));
     }
   }
