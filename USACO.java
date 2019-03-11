e7916c150c9ae6fc9fd74608be911fe46ef87e41
cwu155 <cwu17@stuy.edu>
2019-03-11 00:13:28 -0400

    Shortened readFile

--- USACO.java
+++ USACO.java
@@ -5,17 +5,15 @@ public class USACO{
 
   public static int[][] map;
   public static int r, c, e, n, rows, cols;
-  public static String[] firstLineArray;
   public static String line;
 
   public static int[] stringToInt(String input){
     String[] stringArray = input.split(" ");
     int[] result = new int[stringArray.length];
 
-    for (int i = 0; i < stringArray.length; i++){
-      result[i] = Integer.parseInt(stringArray[i]);
-    }
-
+      for (int i = 0; i < stringArray.length; i++){
+        result[i] = Integer.parseInt(stringArray[i]);
+      }
     return result;
   }
 
@@ -26,51 +24,59 @@ public class USACO{
 
       BufferedReader brTest = new BufferedReader(new FileReader(filename));
       String firstLine = brTest.readLine();
+      String[] firstLineArray = new String[4];
       firstLineArray = firstLine.split(" ");
       r = Integer.parseInt(firstLineArray[0]);
       c = Integer.parseInt(firstLineArray[1]);
       e = Integer.parseInt(firstLineArray[2]);
       n = Integer.parseInt(firstLineArray[3]);
 
+      map = new int[r][c];
+
       inf.nextLine();
-        while(inf.hasNextLine()){
-           line = inf.nextLine();
-           System.out.println(line);
-           rows += 1;
+      for (int i = 0; i < r; i ++) {
+        for (int j = 0; j < c; j++) {
+          map[i][j] = inf.nextInt();
         }
+      }
 
-        map = new int[rows][];
-        rows = 0;
-
-      File text1 = new File(filename);
-      Scanner inf1 = new Scanner(text1);
-
-      inf1.nextLine();
-         while(inf1.hasNextLine()){
-           String next = inf1.nextLine();
-           map[rows] = stringToInt(next);
-           rows += 1;
-         }
+      // File text1 = new File(filename);
+      // Scanner inf1 = new Scanner(text1);
+      //
+      // inf1.nextLine();
+      //    while(inf1.hasNextLine()){
+      //      String next = inf1.nextLine();
+      //      map[r] = stringToInt(next);
+      //      if (map[r].length == 3){;}
+      //      r += 1;
+      //    }
 
       } catch (FileNotFoundException ex){
         System.out.println("Yikes");
       } catch (IOException ex){
         System.out.println("Yikes");
       }
-    }
+  }
 
-    public String toString(){
+    //Debug purposes
+    public static String toString(int[][] map){
       String result = "";
-        for (int i = 0; i < map.length; i++){
-          for (int j = 0; j < map[0].length; j++){
-            result += map[i][j];
+      for (int i = 0; i < map.length; i++){
+          for (int j = 0; j < map[i].length; j++){
+
+            if (j == map[i].length - 1){
+              result += map[i][j] + "\n";
+              } else {
+              result += map[i][j] + " ";
+              }
+            }
           }
-          result += '\n';
-        }
         return result;
       }
 
+
     public static void main(String[] args)throws FileNotFoundException, IOException{
       readFile("makelake.in");
+      System.out.println(toString(map));
     }
   }
